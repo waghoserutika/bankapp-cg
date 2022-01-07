48b991e1fe32cafeb938e1b96b7cddc1e8d68d28
Rutika Waghose <waghoserutika17@gmail.com>
2022-01-07 15:23:17 +0530

    initial commit

---
+++ src/main/java/com/springboot/bankapp/controller/AccountController.java
@@ -0,0 +1,13 @@
+package com.springboot.bankapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+	@GetMapping("/test")
+	public String test() {
+		return "works!!";
+	}
+}
